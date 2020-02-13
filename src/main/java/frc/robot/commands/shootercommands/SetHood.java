60542266b973de86a4c8f9efe3d3f9b1c9630abd
John William Bowen <johnwb1776@gmail.com>
2020-02-13 16:28:48 -0500

    Changed SetHood to InstantCommand

--- src/main/java/frc/robot/commands/shootercommands/SetHood.java
+++ src/main/java/frc/robot/commands/shootercommands/SetHood.java
@@ -7,48 +7,30 @@
 
 package frc.robot.commands.shootercommands;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.TurretSubsystem;
 
-public class SetHood extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class SetHood extends InstantCommand {
   private final TurretSubsystem m_TurretSubsystem;
-  private boolean finished;
 
   /**
    * Creates a new SetHoodLow.
    */
   public SetHood(TurretSubsystem turretSubsystem) {
     m_TurretSubsystem = turretSubsystem;
-    finished = false;
     addRequirements(m_TurretSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
     if(m_TurretSubsystem.isHoodHigh()){
       m_TurretSubsystem.setHoodLow();
     }else{
       m_TurretSubsystem.setHoodHigh();
     }
-    finished = true;
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return finished;
   }
 }
