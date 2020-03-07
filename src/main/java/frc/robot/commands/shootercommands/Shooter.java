3911393b04bab5234bbe001544ea172e9e13ae2b
John William Bowen <johnwb1776@gmail.com>
2020-03-07 11:49:35 -0500

    Motor stop Commands Removed

--- src/main/java/frc/robot/commands/shootercommands/Shooter.java
+++ src/main/java/frc/robot/commands/shootercommands/Shooter.java
@@ -36,6 +36,7 @@ public class Shooter extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_turretSubsystem.setFlyWheelSpeed(0);
   }
 
   // Returns true when the command should end.
